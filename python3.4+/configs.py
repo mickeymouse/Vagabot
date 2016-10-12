9bf15426cdb9801b5aef53e37249ee6ee5a64ff1
= <=>
2016-10-12 15:00:45 +0000

    anonymizing some data

--- python3.4+/configs.py
+++ python3.4+/configs.py
@@ -2,7 +2,7 @@
 #that are needed by the bot in order to connect correctly to discord.
 
 Configs = {
-    'name' : 'Vagabot', #name of the bot
-    'clientId' : '226525061375459328', #Client Id of the bot
-    'token' : 'MjI2NTI1MDYxMzc1NDU5MzI4.Cr6Xew.8VJqVb6uixdc_9gL7D_p7an5xJE' #Token needed to connect in discord
+    'name' : '', #name of the bot
+    'clientId' : '', #Client Id of the bot
+    'token' : '' #Token needed to connect in discord
 }
